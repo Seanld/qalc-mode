d35f263ac54b44fb93fdc4e83fdead9ff2e57370
Sean Wilkerson <sean@bucketwonders.com>
2023-03-29 16:01:15 -0600

    (not alpha) not working

--- qalc-mode.el
+++ qalc-mode.el
@@ -120,10 +120,8 @@ And `reverse' can be:
                                                       (split-string unit-string ",")))
                                             qalc--all-unit-strings)))
 
-(setq qalc--units-regex (let ((units-regex '((not alpha)
-                                             (append '(or)
-                                                   qalc--all-units)
-                                             (not alpha)))
+(setq qalc--units-regex (let ((units-regex (append '(or)
+                                                   qalc--all-units)))
                           (macroexpand `(rx ,units-regex))))
 
 (defvar qalc-mode-map
